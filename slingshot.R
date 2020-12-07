0854878a9ac490a267258a03073cf224f608a8c1
ishrat98 <bsse0927@iit.du.ac.bd>
2020-12-07 12:02:55 +0600

    sometimes works,sometimes doesnt work

--- slingshot.R
+++ slingshot.R
@@ -12,15 +12,17 @@ cdScFiltAnnotK <- loadHDF5SummarizedExperiment(dir="updated app/cdScFiltAnnotHDF
 cdScFiltAnnot <-  as(cdScFiltAnnotK, "SingleCellExperiment")
 
 
-dim(cdScFiltAnnot)
-dimnames(cdScFiltAnnot)
-View(cdScFiltAnnot)
-counts <- assays(cdScFiltAnnot)$counts
-View(counts)
+ dim(cdScFiltAnnot)
+ dimnames(cdScFiltAnnot)
+ View(cdScFiltAnnot)
+ counts <- assays(cdScFiltAnnot)$counts
+ View(counts)
 
-#data('slingshotExample')
+
 rd <- cdScFiltAnnot$rd
 cl <- cdScFiltAnnot$cl
+
+View(rd)
 condition <- factor(rep(c('A','B'), length.out = nrow(rd)))
 condition[110:140] <- 'A'
 ls()
@@ -34,3 +36,49 @@ sds
 plot(rd, asp = 1, pch = 16, col = brewer.pal(3,'Set1')[condition], las=1)
 lines(sds, lwd=3)
 legend('topleft','(x,y)',legend = c('A','B'), title = 'Condition', pch=16, col = brewer.pal(3,'Set1')[1:2])
+
+n <- nrow(rd); L <- ncol(slingPseudotime(sds))
+noise <- runif(n, -.1,.1)
+plot(as.numeric(slingPseudotime(sds)), rep(1:L, each=n)+noise,pch=16, col = brewer.pal(9,'Set1')[condition], axes=FALSE, xlab='Pseudotime', ylab='Lineage', las=1)
+axis(1); axis(2, at=1:L, las=1)
+
+
+t1 <- slingPseudotime(sds, na=FALSE)[,1]
+w1 <- slingCurveWeights(sds)[,1]
+d1 <- weighted.mean(t1[condition=='A'], w1[condition=='A']) - 
+  weighted.mean(t1[condition=='B'], w1[condition=='B'])
+dist1 <- replicate(1e4, {
+  condition.i <- sample(condition)
+  weighted.mean(t1[condition.i=='A'], w1[condition.i=='A']) - 
+    weighted.mean(t1[condition.i=='B'], w1[condition.i=='B'])
+})
+
+t1 <- slingPseudotime(sds, na=FALSE)[,1]
+w1 <- slingCurveWeights(sds)[,1]
+d1 <- weighted.mean(t1[condition=='A'], w1[condition=='A']) - 
+  weighted.mean(t1[condition=='B'], w1[condition=='B'])
+dist1 <- replicate(1e4, {
+  condition.i <- sample(condition)
+  weighted.mean(t1[condition.i=='A'], w1[condition.i=='A']) - 
+    weighted.mean(t1[condition.i=='B'], w1[condition.i=='B'])
+})
+t2 <- slingPseudotime(sds, na=FALSE)[,2]
+w2 <- slingCurveWeights(sds)[,2]
+d2 <- weighted.mean(t2[condition=='A'], w2[condition=='A']) - 
+  weighted.mean(t2[condition=='B'], w2[condition=='B'])
+dist2 <- replicate(1e4, {
+  condition.i <- sample(condition)
+  weighted.mean(t2[condition.i=='A'], w2[condition.i=='A']) - 
+    weighted.mean(t2[condition.i=='B'], w2[condition.i=='B'])
+})
+
+layout(matrix(1:2,nrow = 1))
+hist(dist1, breaks=50, col='grey50', xlim = range(c(d1,dist1)), probability = TRUE, xlab = 'Difference of Weighted Means', main = 'Lineage 1 Permutation Test', las=1)
+abline(v=d1, col=2, lwd=2)
+legend('topright','(x,y)',legend = c('Null Distn.','Observed'), fill=c('grey50',NA), border=c(1,NA), lty=c(NA,1), lwd=c(NA,2), col=c(NA,2), merge = TRUE, seg.len = .6)
+
+hist(dist2, breaks=50, col='grey50', xlim = range(c(d2,dist2)), probability = TRUE, xlab = 'Difference of Weighted Means', main = 'Lineage 2 Permutation Test', las=1)
+abline(v=d2, col=2, lwd=2)
+legend('topright','(x,y)',legend = c('Null Distn.','Observed'), fill=c('grey50',NA), border=c(1,NA), lty=c(NA,1), lwd=c(NA,2), col=c(NA,2), merge = TRUE, seg.len = .6)
+layout(1)
+
