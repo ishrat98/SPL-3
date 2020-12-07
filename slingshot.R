3b523824f78eabe5ebedf0c8d06b9b07ae230add
ishrat98 <bsse0927@iit.du.ac.bd>
2020-12-07 11:42:53 +0600

    & its working!!!!!jadu!!jadu!!!

--- slingshot.R
+++ slingshot.R
@@ -18,3 +18,19 @@ View(cdScFiltAnnot)
 counts <- assays(cdScFiltAnnot)$counts
 View(counts)
 
+#data('slingshotExample')
+rd <- cdScFiltAnnot$rd
+cl <- cdScFiltAnnot$cl
+condition <- factor(rep(c('A','B'), length.out = nrow(rd)))
+condition[110:140] <- 'A'
+ls()
+
+plot(rd, asp = 1, pch = 16, col = brewer.pal(3,'Set1')[condition], las=1)
+legend('topleft','(x,y)',legend = c('A','B'), title = 'Condition', pch=16, col = brewer.pal(3,'Set1')[1:2])
+
+sds <- cdScFiltAnnot(rd, cl)
+sds
+
+plot(rd, asp = 1, pch = 16, col = brewer.pal(3,'Set1')[condition], las=1)
+lines(sds, lwd=3)
+legend('topleft','(x,y)',legend = c('A','B'), title = 'Condition', pch=16, col = brewer.pal(3,'Set1')[1:2])
