08b04fd8d180cf083d30218c1d3a849c7e5a57bf
ishrat98 <bsse0927@iit.du.ac.bd>
2020-12-07 14:52:13 +0600

    slingshot experiment loading

--- app.R
+++ app.R
@@ -1,67 +1,51 @@
-
-library(scater)
-library(plotly)
-library(reshape2)
-library(circlize)
-library(sSeq)
-library(shinycssloaders)
-library(SingleCellExperiment)
-library(TSCAN)
-library(M3Drop)
-library(monocle)
-library(destiny)
-library(scater)
-library(ggplot2)
-library(ggthemes)
-library(ggbeeswarm)
-library(corrplot)
-library(Polychrome)
 library(slingshot)
-library(SLICER)
-library(ouija)
-set.seed(1)
+library(tidyverse)
+library(tidymodels)
+library(Seurat)
+library(scales)
+library(viridis)
+library(Matrix)
+library(SingleCellExperiment)
+
+library(HDF5Array)
+library(edgeR)
 
 
-deng_SCE <- readRDS("data/deng/deng-reads.rds")
+cdScFiltAnnotK <- loadHDF5SummarizedExperiment(dir="updated app/cdScFiltAnnotHDF5", prefix="")
+#KData <- readRDS("D:/SPL3/Single_cell_rnaseq/data/se.rds")
 
-deng_SCE$cell_type2 <- factor(
-  deng_SCE$cell_type2,
-  levels = c("zy", "early2cell", "mid2cell", "late2cell",
-             "4cell", "8cell", "16cell", "earlyblast",
-             "midblast", "lateblast")
-)
-cellLabels <- deng_SCE$cell_type2
-deng <- counts(deng_SCE)
-colnames(deng) <- cellLabels
 
-deng_SCE <- scater::runPCA(deng_SCE,ncomponent = 5)
+cdScFiltAnnot <-  as(cdScFiltAnnotK, "SingleCellExperiment")
 
-## change color Palette with library(Polychrome)
 
-set.seed(723451) # for reproducibility
-my_color <- createPalette(10, c("#010101", "#ff0000"), M=1000)
-names(my_color) <- unique(as.character(deng_SCE$cell_type2))
+dim(cdScFiltAnnot)
+counts <- assays(cdScFiltAnnot)$counts
+seu <- CreateSeuratObject(counts) %>% 
+  SCTransform() # normalize and scale
+# Add cell type annotation to metadata
+seu <- AddMetaData(seu, setNames(cdScFiltAnnot$labels[ind], cells_use), 
+                   col.name = "cell_type")
 
-pca_df <- data.frame(PC1 = reducedDim(deng_SCE,"PCA")[,1],
-                     PC2 = reducedDim(deng_SCE,"PCA")[,2],
-                     cell_type2 = deng_SCE$cell_type2)
+sds <- slingshot(Embeddings(seu, "umap"), clusterLabels = seu$seurat_clusters, 
+                 start.clus = 4, stretch = 0)
 
-ggplot(data = pca_df)+geom_point(mapping = aes(x = PC1, y = PC2, colour = cell_type2))+
-  scale_colour_manual(values = my_color)+theme_classic()
 
-#deng_SCE$PC1 <- reducedDim(deng_SCE, "PCA")[,1]
+cell_pal <- function(cell_vars, pal_fun,...) {
+  if (is.numeric(cell_vars)) {
+    pal <- pal_fun(100, ...)
+    return(pal[cut(cell_vars, breaks = 100)])
+  } else {
+    categories <- sort(unique(cell_vars))
+    pal <- setNames(pal_fun(length(categories), ...), categories)
+    return(pal[cell_vars])
+  }
+}
 
-ggplot(pca_df, aes(x = PC1, y = cell_type2, 
-                   colour = cell_type2)) +
-  geom_quasirandom(groupOnX = FALSE) +
-  scale_colour_manual(values = my_color) + theme_classic() +
-  xlab("First principal component") + ylab("Timepoint") +
-  ggtitle("Cells ordered by first principal component")
-procdeng <- TSCAN::preprocess(counts(deng_SCE))
+cell_colors <- cell_pal(seu$cell_type, brewer_pal("qual", "Set2"))
+cell_colors_clust <- cell_pal(seu$seurat_clusters, hue_pal())
 
-colnames(procdeng) <- 1:ncol(deng_SCE)
+plot(reducedDim(sds), col = cell_colors, pch = 16, cex = 0.5)
+lines(sds, lwd = 2, type = 'lineages', col = 'black')
 
-dengclust <- TSCAN::exprmclust(procdeng, clusternum = 10)
-
-TSCAN::plotmclust(dengclust)
-
+plot(reducedDim(sds), col = cell_colors_clust, pch = 16, cex = 0.5)
+lines(sds, lwd = 2, type = 'lineages', col = 'black')
