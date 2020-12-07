964a309e8b04d8ee8986c407cff1636f5cb7ae93
ishrat98 <bsse0927@iit.du.ac.bd>
2020-12-07 11:30:28 +0600

    resolved the issues

--- slingshot.R
+++ slingshot.R
@@ -2,13 +2,19 @@ library(monocle) # Load Monocle
 library(SingleCellExperiment)
 library(slingshot)
 library(HDF5Array)
+library(edgeR)
 
-library(rafalib)
 
 cdScFiltAnnotK <- loadHDF5SummarizedExperiment(dir="updated app/cdScFiltAnnotHDF5", prefix="")
 #KData <- readRDS("D:/SPL3/Single_cell_rnaseq/data/se.rds")
 
 
 cdScFiltAnnot <-  as(cdScFiltAnnotK, "SingleCellExperiment")
+
+
+dim(cdScFiltAnnot)
+dimnames(cdScFiltAnnot)
 View(cdScFiltAnnot)
+counts <- assays(cdScFiltAnnot)$counts
+View(counts)
 
