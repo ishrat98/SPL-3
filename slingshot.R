959847647496460d7000e72f3afbba75b4e0af56
ishrat98 <bsse0927@iit.du.ac.bd>
2020-12-12 16:48:35 +0600

    Error in plot.window(...) : need finite 'xlim' values

--- slingshot.R
+++ slingshot.R
@@ -12,50 +12,14 @@ cdScFiltAnnot <-  as(cdScFiltAnnotK, "SingleCellExperiment")
 
 dim(cdScFiltAnnot)
 
-
-
-counts <- assays(cdScFiltAnnot)$counts
-
-dim(counts)
-
-
-rownames(counts) <- paste0('G',1:12022)
-colnames(counts) <- paste0('c',1:1741)
-sim <- SingleCellExperiment(assays = List(counts = counts))
-sim
-View(sim)
-geneFilter <- apply(assays(sim)$counts,1,function(x){
-  sum(x >= 3) >= 10
-})
-sim <- sim[geneFilter, ]
-FQnorm <- function(counts){
-  rk <- apply(counts,2,rank,ties.method='min')
-  counts.sort <- apply(counts,2,sort)
-  refdist <- apply(counts.sort,1,median)
-  norm <- apply(rk,2,function(r){ refdist[r] })
-  rownames(norm) <- rownames(counts)
-  return(norm)
-}
-assays(sim)$norm <- FQnorm(assays(sim)$counts)
+data(cdScFiltAnnot)
 
 rd <- cdScFiltAnnot$rd
 cl <- cdScFiltAnnot$cl
-dim(rd) # data representing cells in a reduced dimensional space
-length(cl) # vector of cluster labels
-View(rd)
+condition <- factor(rep(c('A','B'), length.out = nrow(rd)))
+condition[110:140] <- 'A'
+ls()
 
-pca <- prcomp(t(log1p(assays(sim)$norm)), scale. = FALSE)
-rd1 <- pca$x[,1:2]
-
-plot(rd1, col = rgb(0,0,0,.5), pch=16, asp = 1)
-
-sim5 <- slingshot(sim, clusterLabels = 'GMM', reducedDim = 'PCA',
-                  approx_points = 5)
-
-colors <- colorRampPalette(brewer.pal(11,'Spectral')[-6])(100)
-plotcol <- colors[cut(sim5$slingPseudotime_1, breaks=100)]
-
-plot(reducedDims(sim5)$PCA, col = plotcol, pch=16, asp = 1)
-lines(cdScFiltAnnot(sim5), lwd=2, col='black')
-
+plot(rd, asp = 1, pch = 16, col = brewer.pal(3,'Set1')[condition], las=1)
+legend('topleft','(x,y)',legend = c('A','B'), title = 'Condition', pch=16, col = brewer.pal(3,'Set1')[1:2])
 
