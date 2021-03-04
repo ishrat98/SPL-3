0f9de1d39faa346f09af3f60e102f0b602388418
ishrat98 <bsse0927@iit.du.ac.bd>
2021-03-04 12:19:36 +0600

    DE for large dataset edded

--- slingshot.R
+++ slingshot.R
@@ -51,3 +51,37 @@ plotSmoothers(sce, counts, gene = sigGeneStart)
 
 plotGeneCount(crv, counts, gene = sigGeneStart)
 
+customRes <- startVsEndTest(sce, pseudotimeValues = c(0.1, 0.8))
+
+library(clusterExperiment)
+nPointsClus <- 20
+clusPat <- clusterExpressionPatterns(sce, nPoints = nPointsClus,
+                                     genes = rownames(counts)[1:100])
+clusterLabels <- primaryCluster(clusPat$rsec)
+
+cUniq <- unique(clusterLabels)
+cUniq <- cUniq[!cUniq == -1] # remove unclustered genes
+
+for (xx in cUniq[1:4]) {
+  cId <- which(clusterLabels == xx)
+  p <- ggplot(data = data.frame(x = 1:nPointsClus,
+                                y = rep(range(clusPat$yhatScaled[cId, ]),
+                                        nPointsClus / 2)),
+              aes(x = x, y = y)) +
+    geom_point(alpha = 0) +
+    labs(title = paste0("Cluster ", xx),  x = "Pseudotime", y = "Normalized expression") +
+    theme_classic() +
+    theme(plot.title = element_text(hjust = 0.5))
+  for (ii in 1:length(cId)) {
+    geneId <- rownames(clusPat$yhatScaled)[cId[ii]]
+    p <- p +
+      geom_line(data = data.frame(x = rep(1:nPointsClus, 2),
+                                  y = clusPat$yhatScaled[geneId, ],
+                                  lineage = rep(0:1, each = nPointsClus)),
+                aes(col = as.character(lineage), group = lineage), lwd = 1.5)
+  }
+  p <- p + guides(color = FALSE) +
+    scale_color_manual(values = c("orange", "darkseagreen3"),
+                       breaks = c("0", "1"))  
+  print(p)
+}
